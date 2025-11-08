045faa2e221c8d995f8141052c08ea021afc2a28
Balázs Szabó <szabobalazs455@mailbox.unideb.hu>
2025-11-08 19:46:13 +0100

    Fix: added currentcooldown to abilities

--- util/abilities/knightAbilities.lua
+++ util/abilities/knightAbilities.lua
@@ -2,6 +2,7 @@ return {
     ability1 = {
         name = "Crushing Blow",
         cooldown = 2,
+        currentCooldown = 0,
         passive = false,
         effect = function(user)
             return math.floor(user.stats.attack * 1.35 - user.stats.attack)
@@ -10,6 +11,7 @@ return {
     ability2 = {
         name = "Berserker's Vow",
         cooldown = 5,
+        currentCooldown = 0,
         passive = false,
         effect = function(user)
             user.effects.berserkTurns = 2
@@ -18,6 +20,7 @@ return {
     ability3 = {
         name = "Battle Cry",
         cooldown = 3,
+        currentCooldown = 0,
         passive = false,
         effect = function(user)
             user.effects.battleCryTurns = 3
@@ -26,6 +29,7 @@ return {
     ability4 = {
         name = "Steel Discipline",
         cooldown = 0,
+        currentCooldown = 0, -- nem tudom megdöglik e nélküle.
         passive = true,
         effect = function(user)
             user.stats.attack = user.stats.attack + 10
@@ -34,6 +38,7 @@ return {
     ability5 = {
         name = "Crushing Blow",
         cooldown = 6,
+        currentCooldown = 0,
         passive = false,
         effect = function(user)
             if math.random() < 0.25 then
